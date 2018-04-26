adf7c5fced4d2f99165b1190568bceba5a276e2f
CodySchrank <cody.schrank@gmail.com>
2018-04-26 13:35:48 -0700

    gravy

--- src/extention.ts
+++ src/extention.ts
@@ -2,6 +2,12 @@
 
 import * as vscode from 'vscode';
 
+/**
+ * 
+ *  The gravy is in package.json -> contributes
+ * 
+ */
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
